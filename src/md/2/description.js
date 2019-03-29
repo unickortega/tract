43090d736e3eb0466280bade97a26e739f6455a3
Unick <uelmar_ortega@yahoo.com>
2019-03-29 09:18:59 +0800

    added 404.html

---
+++ src/md/2/description.js
@@ -0,0 +1,5 @@
+export default {
+    title: 'My First Blog',
+    description: 'For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.',
+    date: 'March 20, 2019'
+}
